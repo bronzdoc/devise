b0d2dabfe03c91e45683176cd501def0388e1c77
Luis Sagastume <lsagastume1990@gmail.com>
2023-09-18 07:04:12 -0600

    Remove on_failure config omniauth.rb

--- lib/devise/omniauth.rb
+++ lib/devise/omniauth.rb
@@ -12,12 +12,12 @@ end
 # Clean up the default path_prefix. It will be automatically set by Devise.
 OmniAuth.config.path_prefix = nil
 
-OmniAuth.config.on_failure = Proc.new do |env|
-  env['devise.mapping'] = Devise::Mapping.find_by_path!(env['PATH_INFO'], :path)
-  controller_name  = ActiveSupport::Inflector.camelize(env['devise.mapping'].controllers[:omniauth_callbacks])
-  controller_klass = ActiveSupport::Inflector.constantize("#{controller_name}Controller")
-  controller_klass.action(:failure).call(env)
-end
+#OmniAuth.config.on_failure = Proc.new do |env|
+#  env['devise.mapping'] = Devise::Mapping.find_by_path!(env['PATH_INFO'], :path)
+#  controller_name  = ActiveSupport::Inflector.camelize(env['devise.mapping'].controllers[:omniauth_callbacks])
+#  controller_klass = ActiveSupport::Inflector.constantize("#{controller_name}Controller")
+#  controller_klass.action(:failure).call(env)
+#end
 
 module Devise
   module OmniAuth
